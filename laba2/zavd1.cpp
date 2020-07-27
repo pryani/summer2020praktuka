e120ff65be66f494b00b82556724c3a1fcb51b3a
pryani <68861461+pryani@users.noreply.github.com>
2020-07-27 18:44:54 +0300

    Add files via upload

---
+++ laba2/zavd1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main(int argc, const char * argv[]) {
+	setlocale(LC_ALL, "Russian");
+    int v[11];
+    int sum, dob;
+    sum=0;
+    dob=1;
+    for (int i=1; i<=11; i++) {
+        v[i]= (rand()% 50 + 1);
+        cout<<v[i]<<"   ";
+        if (i%2 ==0) {
+            sum+=v[i];
+        } else {
+            dob=dob*v[i];
+        }
+    };
+    cout<<endl<<"Сума елементiв що мають парний iндекс : "<<sum<<endl;
+    cout<<"Добуток елементiв що мають не парний iндекс: "<<dob<<"     ";
+    return 0;
+}
