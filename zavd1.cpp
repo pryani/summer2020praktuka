8c8ee19496e6eacd14f31c9abc73d9282e38dd1d
pryani <68861461+pryani@users.noreply.github.com>
2020-07-27 18:27:43 +0300

    Add files via upload

---
+++ zavd1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+	setlocale(LC_ALL, "Russian");
+    double x,y;
+    cout<<"Введить координати точки(х,y): ";
+    cin>>x>>y;
+    cout<<"";
+    if (x>=-3 && x<=0 && y>=-1 && y<=1 && y<=x+2 && y>=x-1 ) {
+        cout<<"Точка входить в область";
+    } else {
+        cout<<"Точка не входить в область";
+
+    }
+    return 0;
+}
