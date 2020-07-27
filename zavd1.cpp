48314fa4c222cecb1bd4e3f3a2b68527d76b2222
pryani <68861461+pryani@users.noreply.github.com>
2020-07-27 18:26:42 +0300

    laba2

--- zavd1.cpp
+++ zavd1.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
-
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 int main(int argc, const char * argv[]) {
 	setlocale(LC_ALL, "Russian");
-    double x,y;
-    cout<<"Введить координати точки(х,y): ";
-    cin>>x>>y;
-    cout<<"";
-    if (x>=-3 && x<=0 && y>=-1 && y<=1 && y<=x+2 && y>=x-1 ) {
-        cout<<"Точка входить в область";
-    } else {
-        cout<<"Точка не входить в область";
-
-    }
+    int v[11];
+    int sum, dob;
+    sum=0;
+    dob=1;
+    for (int i=1; i<=11; i++) {
+        v[i]= (rand()% 50 + 1);
+        cout<<v[i]<<"   ";
+        if (i%2 ==0) {
+            sum+=v[i];
+        } else {
+            dob=dob*v[i];
+        }
+    };
+    cout<<endl<<"Сума елементiв що мають парний iндекс : "<<sum<<endl;
+    cout<<"Добуток елементiв що мають не парний iндекс: "<<dob<<"     ";
     return 0;
 }
