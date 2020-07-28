ead07275af95529ab7c246094f95d24c3f2fad0e
pryani <68861461+pryani@users.noreply.github.com>
2020-07-28 20:43:41 +0300

    Add files via upload

---
+++ laba5/zavd1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{   setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "¬вед≥ть розм≥р масиву:" << endl;
+    cin >> n;
+    int * a = new int[n];
+    
+    for(int i = 0; i < n;i++)
+    {
+        cout << "¬вед≥ть a[" << i << "]\t";
+        cin >> a[i];
+    }
+    for(int *i = a; i < (a+n);i = (i+2))
+        for(int *j = i; j < (a+n);j = (j+2))
+            if (*i > *j)
+                swap(*i,*j);
+    for(int *i = (a+1); i < (a+n);i = (i+2))
+        for(int *j = i; j < (a+n);j = (j+2))
+            if (*i < *j)
+                swap(*i,*j);
+    for(int i = 0; i < n;i++)
+        cout << a[i] << ' ';
+    delete [] a;
+    return 0;
+}
