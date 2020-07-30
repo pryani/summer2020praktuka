698288c6451ce95b7da3b0262f5592df517e1656
pryani <68861461+pryani@users.noreply.github.com>
2020-07-30 12:51:48 +0300

    Add files via upload

---
+++ laba6/zavd1/zavd1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <random>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    setlocale(0, "ukrainian");
+    const int t = 6;
+    int sum[6];
+    for (int i(0); i < 6; i++)
+        sum[i] = 0;
+    int tmp;
+    cout << "|1|2|3|4|5|" << endl;
+    for (int i(0); i < 6; i++)
+    {
+        cout << "__" << i + 1 << "|";
+        for (int j(0); j < 5; j++)
+        {
+            cout.width(5);
+            cout.fill('_');
+            tmp = rand() % (10 * t + 50);
+            sum[i] += tmp;
+            cout << tmp << "|";
+        }
+        cout << endl;
+    }
+    int mxind = 0;
+    for (int i(1); i < 6; i++)
+        if (sum[mxind] < sum[i])
+            mxind = i;
+    cout << endl << "у " << mxind + 1 << " населеному пункті проголосувало найбільше людей" << endl;
+    return 0;
+}
---
+++ laba6/zavd2/zavd2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <stdio.h>
+
+using namespace std;
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    setlocale(LC_ALL, "Russian");
+    char stringg[50];
+    cout << "Введіть ПІБ" << endl;
+    gets_s(stringg);
+    double length;
+    length = strlen(stringg);
+    cout << "Довжина рядка: " << length << endl;
+    int a;
+    a = 0;
+    for (int i = 0; i < length; i++) {
+        if (stringg[i] == 'a') {
+            a += 1;
+        }
+    }
+    cout << "Кількість букв А: " << a << endl;
+    char mas[20];
+    for (int i = 0; i < length; i++) {
+        if (stringg[i] == ' ') {
+            break;
+        }
+        mas[i] = stringg[i];
+    }
+    string str = string(mas);
+    reverse(str.begin(), str.end());
+    string name = string(stringg);
+
+    for (int i = 0; i < name.length(); i++) {
+        name.erase(i, 1);
+        if (name[i] == ' ') {
+            break;
+        }
+    }
+    name.erase(0, 4);
+    int count = 0;
+    for (int i = 0; i < name.length(); i++) {
+        if (name[i] == ' ') {
+            break;
+        }
+        count += 1;
+    }
+   // count += 4;
+    cout << str << " " << endl;
+    cout << "Кількість букв в імені: " << count << endl;
+    return 0;
+}
