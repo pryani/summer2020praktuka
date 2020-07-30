0e05621649e3ecdbe4026008754c1800a1792000
pryani <68861461+pryani@users.noreply.github.com>
2020-07-30 11:22:40 +0300

    Add files via upload

---
+++ laba3/zavd1/zavd1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    setlocale(LC_ALL, "Russian");
+    int k[3][6];
+    int kilk;
+    kilk = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 6; j++) {
+            k[i][j] = rand() % 109 + (-37);
+            cout << k[i][j] << "   ";
+            if (k[i][j] < 0) {
+                kilk += 1;
+            }
+        }
+        cout << endl;
+    }
+    cout << endl << "Кiлькість вiд'ємних елементiв: " << kilk;
+    return 0;
+}
