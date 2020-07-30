127963132a6f06071521aa2e28077a84b6551f50
pryani <68861461+pryani@users.noreply.github.com>
2020-07-30 13:18:01 +0300

    Add files via upload

---
+++ laba9/zavd1/zavd1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    int n;
+    cout << "Введіть N: " << endl;
+    cin >> n;
+    int** arr = new int*[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new int[1];
+    }
+    int plus[1];
+    int minus[1];
+    for (int i = 0; i < n; i++) {
+        plus[i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        minus[i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = rand() % 109 + (-37);
+            cout << arr[i][j] << "\t" << "\t";
+            if (arr[i][j] > 0) {
+                plus[j] = 1;
+            }
+            else if (arr[i][j] < 0) {
+                minus[j] = 1;
+            }
+        }
+        cout << endl;
+    }
+    cout << endl << rand() % n << endl;
+    delete[] arr;
+    return 0;
+}
