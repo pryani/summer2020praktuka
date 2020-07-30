85461945c06939856b9555e3a0298c80360687ac
pryani <68861461+pryani@users.noreply.github.com>
2020-07-30 12:58:32 +0300

    Add files via upload

---
+++ zavd1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <random>
+#include <time.h>
+using namespace std;
+int mass(int k, int i) {
+    srand(time(NULL));
+    int y[k];
+    for (int j = 0; j < k; j++) {
+        y[j] = rand() % 109 + (-37);
+        i++;
+        cout << y[j] << "   ";
+    }
+    int dob, sum;
+    dob = 1;
+    sum = 0;
+    for (int j = 0; j < k; j++) {
+        if (y[j] > 0) {
+            sum += 1;
+        }
+        else {
+            dob *= y[j];
+        }
+    }
+    cout << endl << "Добуток: " << dob;
+    cout << endl << "Кількість: " << sum << endl;
+    return 0;
+}
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    setlocale(0, "ukrainian");
+    int k, i;
+    cout << "Введіть K(від 1 до 12): ";
+    cin >> k;
+    cout << "Введіть номер варіанту: ";
+    cin >> i;
+    mass(k, i);
+    return 0;
+}
