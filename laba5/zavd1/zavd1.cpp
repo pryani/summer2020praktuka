66f94fc1480699a71ccbfdb1a4e43f35534326b4
pryani <68861461+pryani@users.noreply.github.com>
2020-07-30 11:32:37 +0300

    Add files via upload

---
+++ laba5/zavd1/zavd1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введіть розмір масиву:" << endl;
+    cin >> n;
+    int* a = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Введіть a[" << i << "]\t";
+        cin >> a[i];
+    }
+    for (int* i = a; i < (a + n); i = (i + 2))
+        for (int* j = i; j < (a + n); j = (j + 2))
+            if (*i > * j)
+                swap(*i, *j);
+    for (int* i = (a + 1); i < (a + n); i = (i + 2))
+        for (int* j = i; j < (a + n); j = (j + 2))
+            if (*i < *j)
+                swap(*i, *j);
+    for (int i = 0; i < n; i++)
+        cout << a[i] << ' ';
+    delete[] a;
+    return 0;
+}
