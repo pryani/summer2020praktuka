878fac3d0fca4a1d1fba21c7e1f5d07e24406ef7
pryani <68861461+pryani@users.noreply.github.com>
2020-07-30 13:00:19 +0300

    Add files via upload

---
+++ laba8/zavd1/zavd1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+void krug(int x, int y, int r) {
+    if (pow(x, 2) + pow(y, 2) <= pow(r, 2)) {
+        cout << "Точка входить в круг " << endl;
+    }
+    else {
+        cout << "Точка не входить в круг " << endl;
+    }
+}
+int main(int argc, const char* argv[]) {
+    setlocale(0, "ukrainian");
+    // insert code here...
+    int x, y, r;
+    cout << "Введіть X, Y, R: " << endl;
+    cin >> x >> y >> r;
+    krug(x, y, r);
+    return 0;
+}
