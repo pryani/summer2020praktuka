48157f0f01be46ec71807fcb2ca28d3462f40f20
pryani <68861461+pryani@users.noreply.github.com>
2020-07-30 13:18:39 +0300

    Update zavd1.cpp

--- laba9/zavd1/zavd1.cpp
+++ laba9/zavd1/zavd1.cpp
@@ -8,10 +8,10 @@ int main(int argc, const char* argv[]) {
     cin >> n;
     int** arr = new int*[n];
     for (int i = 0; i < n; i++) {
-        arr[i] = new int[1];
+        arr[i] = new int[n];
     }
-    int plus[1];
-    int minus[1];
+    int plus[n];
+    int minus[n];
     for (int i = 0; i < n; i++) {
         plus[i] = 0;
     }
