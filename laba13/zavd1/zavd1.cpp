6a48feed4f9bc219976b459977030baf793e0de8
pryani <68861461+pryani@users.noreply.github.com>
2020-08-02 20:22:31 +0300

    Add files via upload

---
+++ laba13/zavd1/zavd1.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    string path = "text.txt";
+    string path2 = "text1.txt";
+    ifstream fin;
+    fin.open(path);
+    ofstream file;
+    file.open(path2);
+    if (!fin.is_open()) {
+        cout << "Error, file is not opened" << endl;
+    }
+    else {
+        string str;
+        while (!fin.eof()) {
+            str = "";
+            getline(fin, str);
+            if (str[str.size() - 1] == '!') {
+                str = "";
+            }
+
+            file << str << "\n";
+
+            cout << str << endl;
+        }
+    }
+    file.close();
+    fin.close();
+    return 0;
+}
---
+++ laba13/zavd2/zavd2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(int argc, const char* argv[]) {
+    int count1;
+    cout << "input count of lines of first text:\t";
+    cin >> count1;
+    vector<string> words_ftext;
+    string temp;
+    cin.ignore();
+    for (int i(0); i < count1; i++)
+    {
+        getline(cin, temp);
+        char delim = ' ';
+        while (temp.find(delim) != -1)
+        {
+            words_ftext.push_back(temp.substr(0, temp.find(delim)));
+            temp.erase(0, temp.find(delim) + 1);
+        }
+        words_ftext.push_back(temp);
+
+    }
+    for (int i(0); i < words_ftext.size(); i++)
+    {
+        while (words_ftext[i].find('.') != -1)
+            words_ftext[i].erase(words_ftext[i].find('.'), 1);
+        while (words_ftext[i].find(',') != -1)
+            words_ftext[i].erase(words_ftext[i].find(','), 1);
+        while (words_ftext[i].find('!') != -1)
+            words_ftext[i].erase(words_ftext[i].find('!'), 1);
+    }
+    int count2;
+    cout << "input count of lines of second text:\t";
+    cin >> count2;
+    cin.ignore();
+    vector<string> text2;
+    for (int i(0); i < count2; i++)
+    {
+        getline(cin, temp);
+        for (int j(0); j < words_ftext.size(); j++)
+            while (temp.find(words_ftext[j]) != -1)
+                temp.erase(temp.find(words_ftext[j]), words_ftext[j].length());
+        text2.push_back(temp);
+    }
+    for (int i(0); i < text2.size(); i++)
+        cout << text2[i] << endl;
+    cout << endl;
+    system("pause");
+    return 0;
+}
