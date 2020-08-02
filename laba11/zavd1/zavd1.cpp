25360dbeaa32bba0ad4b65418f094087d42743e4
pryani <68861461+pryani@users.noreply.github.com>
2020-08-02 08:55:06 +0300

    Add files via upload

---
+++ laba11/zavd1/zavd1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+struct baginfo
+{
+	unsigned int count;
+	double weight;
+};
+int main()
+{
+
+	baginfo data[24];
+	int sumcount = 0;
+	double sumweight = 0;
+	for (int i(0); i < 24; i++)
+	{
+		cout << "passenger #" << i + 1 << endl;
+		cout << "input count:\t";
+		cin >> data[i].count;
+		cout << "input weight:\t";
+		cin >> data[i].weight;
+		sumcount += data[i].count;
+		sumweight += data[i].weight;
+	}
+	double avercount = sumcount / 24;
+	double averweight = sumweight / sumcount;
+	int firstp = 0;
+	for (int i(0); i < 24; i++)
+		if (data[i].count > 2)
+			firstp++;
+	cout << "1.) " << firstp << endl;
+	int secoundp = 0;
+	for (int i(0); i < 24; i++)
+		if (data[i].count == 1 && data[i].weight < 25)
+			secoundp++;
+	cout << "2.) " << secoundp << endl;
+	int thirdp = 0;
+	for (int i(0); i < 24; i++)
+		if (data[i].count > avercount)
+			thirdp++;
+	cout << "3.) " << thirdp << endl;
+	for (int i(0); i < 24; i++)
+		if (abs((data[i].weight / data[i].count) - averweight) < 0.5)
+		{
+			cout << "4.) " << i + 1 << endl;
+			goto metka;
+		}
+	cout << "4.) passenger not found" << endl;
+metka:
+	system("pause");
+	return 0;
+}
---
+++ laba11/zavd2/zavd2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+struct study {
+    int  count;
+    char type;
+};
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int countt;
+    char typee[50];
+    int n, c;
+    c = 0;
+    cout << "введіть кількість навчальних закладів: " << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Введіть кількість учнів:\t";
+        cin >> countt;
+        cout << "Введіть тип навчального закладу:\t";
+        cin >> typee;
+        string str = string(typee);
+        if (str == "school") {
+            c += 1;
+        }
+        str = "";
+    }
+    cout << endl << c << endl;
+    return 0;
+}
